15a76b2c6176c869234f6ed556eedb463b1939ac
Abdulhakim Mukhammadov <abdulhakimmuhammadov55@gmail.com>
2024-09-10 07:17:06 +0500

    Yangi darslar qo'shildi

---
+++ 2-ozgaruvchilar-va-malumot-turlari/sonlar-1-amaliyot.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 13:52:37 2024
+
+@author: Saidus_work
+"""
+
+# Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+
+son = input('Istalgan son kiriting:')
+son = int(son)
+print(son, 'ning kvadrati ', son**2 , ' ga teng.')
+print(son, 'ning kubi ', son**3 , ' ga teng.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
