df2eeb2bcaca9f1ddd6539b56a24a863fe124626
Abdulhakim Mukhammadov <abdulhakimmuhammadov55@gmail.com>
2024-09-10 06:21:01 +0500

    Yangi darslar qo'shildi
    o'zgaruvchilar-va-ma'lumot-turlari papkasida yangi darslar

---
+++ o'zgaruvchilar-va-ma'lumot-turlari/sonlar-2-amaliyot.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 14:08:13 2024
+
+@author: Saidus_work
+"""
+
+# Foydalanuvchining yoshini so'rab, uning tug'ilgan yilini hisoblab, konsolga chiqaruvchi dastur
+
+yosh = input('Yoshingiz nechida : ')
+yosh = int(yosh)
+t_yil = 2024 - yosh
+print(f' Siz {t_yil} - yilda tug\'ulgansiz!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
